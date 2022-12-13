7593f6915364a359eece7b89c887cdcc06017a42
egladw3l <82402996+egladw3l@users.noreply.github.com>
2022-12-14 00:13:35 +0200

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 /**
  * print_to_98 - Prints all natural numbers
  * from n to 98.
- *  
+ *
  * @n: input number.
  *
  * Return: no return.
@@ -21,6 +21,7 @@ void print_to_98(int n)
 		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
-		}							}
+		}
+	}
 	printf("%d\n", n);
 }
