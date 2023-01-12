47fd20067b7d00b5312fa2cd0d66a799990318ef
egladwel <gmadziva14@gmail.com>
2023-01-12 08:19:30 -0800

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int);
-char *string_nconcat(char *, char *, unsigned int);
+char *string_nconcat(char *, char *, unsigned int)
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
 void *_realloc(void *, unsigned int, unsigned int);
